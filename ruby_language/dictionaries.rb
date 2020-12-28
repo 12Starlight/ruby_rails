213b6d2c89498c22b1980ab84d5d98aff2cd6a15
Dave <dave.gagnat@gmail.com>
2020-12-28 15:30:44 -0600

    Added Changing Values In A Hash to dictionaries.rb

--- ruby_language/dictionaries.rb
+++ ruby_language/dictionaries.rb
@@ -35,3 +35,24 @@ puts
 sample_hash.each do |key, value| 
   puts "The class for key is #{key.class} and the value is #{value}"
 end
+puts 
+
+my_details.each do |key, value| 
+  puts "The class for key is #{key.class} and the value is #{value}"
+end
+puts 
+
+another_hash.each do |key, value| 
+  puts "The class for key is #{key.class} and the value is #{value}"
+end
+
+puts "\nChanging Values In A Hash\n"
+40.times { print "-" }
+puts
+
+p another_hash
+another_hash[:a] = "Dave"
+p another_hash
+another_hash.each { |key, val| puts "The key is #{key} and the value is #{val}"}
+p another_hash.select { |k, v| v.is_a?(String)}
+p another_hash.each { |k, v| another_hash.delete(k) if v.is_a?(String)}
