62f3050c5b235e84a1443daf9baa90600d866d09
Dave <dave.gagnat@gmail.com>
2020-12-28 15:12:58 -0600

    Added Sample Hashes to dictionaries.rb

--- ruby_language/dictionaries.rb
+++ ruby_language/dictionaries.rb
@@ -5,6 +5,33 @@
 =begin
 Hash a.k.a. dictionary
 
+my_hash = {}
 sample_hash = { 'a' => 1, 'b' => 2, 'c' => 3 }
 
 =end
+
+puts "\nSample Hash\n"
+40.times { print "-" }
+puts
+
+p sample_hash = { 'a' => 1, 'b' => 2, 'c' => 3 }
+p my_details = { 'name' => 'Dave', 'favcolor' => 'blue' }
+p my_details['favcolor']
+p sample_hash['b']
+
+p sample_hash
+another_hash = { a: 1, b: 2, c: 3 }
+p another_hash
+p another_hash[:a]
+
+puts "\nHash Methods\n"
+40.times { print "-" }
+puts
+
+p sample_hash.keys
+p sample_hash.values
+puts 
+
+sample_hash.each do |key, value| 
+  puts "The class for key is #{key.class} and the value is #{value}"
+end
