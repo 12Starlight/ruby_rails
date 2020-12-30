82d5755b0398018604cef319899432a79b2d74e8
Dave <dave.gagnat@gmail.com>
2020-12-30 17:17:02 -0500

    Added conditionals for helper function to handle edge case

--- ruby_language/hw5_project1_area_codes.rb
+++ ruby_language/hw5_project1_area_codes.rb
@@ -14,19 +14,23 @@ dial_book = {
 # Get city names from the hash
 def get_city_names(somehash)
   puts "What city do you want the area code for?"
-  somehash.each { |k, v| puts k.capitalize }
+  somehash.each { |k, v| puts k.capitalize } # somehash.keys could be used
   code = gets.chomp
   get_area_code(somehash, code.downcase)
 end
 
 # Get area code based on given hash and key # helper
 def get_area_code(somehash, key)
-  puts "The area code for #{key.capitalize} is #{somehash[key]}"
+  if somehash.include?(key) 
+    puts "The area code for #{key.capitalize} is #{somehash[key]}"
+  else 
+    puts "You entered an invalid city name"
+  end
 end
 
 # Execution flow
 loop do 
-  puts "Do you want to lookup an area code based on a city name? (Y/N"
+  puts "Do you want to lookup an area code based on a city name? (Y/N)"
   answer = gets.chomp
   break if answer.downcase != "y"
   get_city_names(dial_book)
