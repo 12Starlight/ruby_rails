f20694bfb99032dc46cad7448f48fe88325be0ee
Dave <dave.gagnat@gmail.com>
2020-12-30 16:46:30 -0500

    Built out logic and first method displaying city names

--- ruby_language/hw5_project1_area_codes.rb
+++ ruby_language/hw5_project1_area_codes.rb
@@ -10,12 +10,21 @@ dial_book = {
   "orlando" => "407",
   "lancaster" => "717"
 }
-
+ 
 # Get city names from the hash
 def get_city_names(somehash)
+  puts "What city do you want the area code for?"
+  somehash.each { |k, v| puts k.capitalize }
 end
 
 # Get area code based on given hash and key
 def get_area_code(somehash, key)
 end
 
+# Execution flow
+loop do 
+  puts "Do you want to lookup an area code based on a city name? (Y/N"
+  answer = gets.chomp
+  break if answer.downcase != "y"
+  get_city_names(dial_book)
+end 
