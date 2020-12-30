fc473813a02600e170bcc8aaed0d9449409d0e30
Dave <dave.gagnat@gmail.com>
2020-12-30 15:10:59 -0500

    Added first project area_codes

---
+++ ruby_language/hw5_project1_area_codes.rb
@@ -0,0 +1,21 @@
+dial_book = {
+  "newyork" => "212",
+  "newbrunswick" => "732",
+  "edison" => "988",
+  "plainsboro" => "689",
+  "sanfrancisco" => "301",
+  "miami" => "305",
+  "paloalto" => "650",
+  "evanston" => "847",
+  "orlando" => "407",
+  "lancaster" => "717"
+}
+
+# Get city names from the hash
+def get_city_names(somehash)
+end
+
+# Get area code based on given hash and key
+def get_area_code(somehash, key)
+end
+
