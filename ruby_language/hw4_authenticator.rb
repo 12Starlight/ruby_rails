6cb380d151b68012c55ef4b0d4292a4f670f0522
Dave <dave.gagnat@gmail.com>
2020-12-30 14:46:45 -0500

    Built authentication project

--- ruby_language/hw4_authenticator.rb
+++ ruby_language/hw4_authenticator.rb
@@ -11,6 +11,40 @@ p "Welcome to the authenticator"
 puts
 puts "This program will take input from the user and compare password"
 puts "If password is correct, you get back the user object"
+
+def auth_user(username, password, list)
+  list.each do |user|
+    if user[:username] == username && user[:password] == password
+      return user
+    end
+  end
+
+  return "Credentials were not correct"
+end 
+ 
+
+attempts = 1
+while attempts < 4
+  print "Username: "
+  username = gets.chomp
+  print "Password: "
+  password = gets.chomp
+  authentication = auth_user(username, password, users)
+  puts authentication
+  puts "Press n to quit or any other key to continue: "
+  input = gets.chomp.downcase
+
+  if input == "n"
+    break
+  else
+    
+  end
+  attempts += 1
+end 
+puts "You have exceed the number of attempts" if attempts == 4
+
+
+=begin
 puts 
 puts "What is your password?"
 password = gets.chomp
@@ -26,3 +60,6 @@ def verify_password(password)
 end
 
 p verify_password("awesome")
+
+=end
+
