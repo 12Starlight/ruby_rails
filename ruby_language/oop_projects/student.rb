9879524b26204594423422511c8837858f784864
Dave <dave.gagnat@gmail.com>
2020-12-31 16:47:40 -0500

    Added shortcut for getters and setters

--- ruby_language/oop_projects/student.rb
+++ ruby_language/oop_projects/student.rb
@@ -1,25 +1,27 @@
 class Student
+  attr_accessor :first_name, :last_name, :email, :username
+
   @first_name
   @last_name
   @email
   @username
   @password
   
-  def first_name=(name)
-    @first_name = name
-  end
+  # def first_name=(name)
+  #   @first_name = name
+  # end
 
-  def first_name
-    @first_name
-  end
+  # def first_name
+  #   @first_name
+  # end
 
-    def last_name=(name)
-    @last_name = name
-  end
+  #   def last_name=(name)
+  #   @last_name = name
+  # end
 
-  def last_name
-    @last_name
-  end
+  # def last_name
+  #   @last_name
+  # end
 
   def to_s
     "First name: #{@first_name}"
