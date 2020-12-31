f01c693013a8d5bc09513e5d45612168d6a6907a
Dave <dave.gagnat@gmail.com>
2020-12-31 16:34:08 -0500

    Added getters and setters

--- ruby_language/oop_projects/student.rb
+++ ruby_language/oop_projects/student.rb
@@ -4,6 +4,22 @@ class Student
   @email
   @username
   @password
+  
+  def first_name=(name)
+    @first_name = name
+  end
+
+  def first_name
+    @first_name
+  end
+
+    def last_name=(name)
+    @last_name = name
+  end
+
+  def last_name
+    @last_name
+  end
 
   def to_s
     "First name: #{@first_name}"
@@ -12,6 +28,7 @@ class Student
 end
 
 Dave = Student.new
-puts Dave
-# Dave.first_name = "Dave Gagnat"
 # puts Dave
+Dave.first_name = "Dave"
+Dave.last_name = "Gagnat"
+puts "#{Dave.first_name} #{Dave.last_name}" 
