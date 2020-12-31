e04a0d5fd035db4a231564d6b7c39ff73dcfb61d
Dave <dave.gagnat@gmail.com>
2020-12-31 14:04:24 -0500

    Added oop.rb

---
+++ ruby_language/oop.rb
@@ -0,0 +1,27 @@
+=begin
+Object Oriented Programming (OOP) is a programming paradigm that uses objects 
+and their interactions to design and program applications.
+  
+
+Examples:
+  University
+
+  Student
+    students - first name, last name, email, student id
+
+  Course
+    courses - course name, course description, course id
+
+  
+  Bookstore
+
+  Book
+    books - ISBN, name, description
+
+  Employee
+    employees - SSN, first name, last name, email, employee id
+
+
+  News Network/Blog Site
+
+=end
